66e8e6f0a4d510718026a444dbd952abaff19d1e
Daisuke Shiraishi <kaiyujin@gmail.com>
2019-11-22 14:43:37 +0900

    first commit

---
+++ src/main/kotlin/kaiyujin/graphql_sample/GraphqlSampleApplication.kt
@@ -0,0 +1,11 @@
+package kaiyujin.graphql_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphqlSampleApplication>(*args)
+}
---
+++ src/test/kotlin/kaiyujin/graphql_sample/GraphqlSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package kaiyujin.graphql_sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
